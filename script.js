f0d004e49b211fff529ea65eba2206b89d441477
Dennis <121116755+denyo177@users.noreply.github.com>
2023-02-07 11:16:29 +0100

    Remove double borders

--- script.js
+++ script.js
@@ -11,6 +11,18 @@ for (i = 0; i < columns; i++) {
         const row = document.createElement('div');
         row.classList.add('row');
         //row.textContent = i + "-" + j;
+        //only set left border except for last column to prevent double borders
+        if (i === 15) {
+            row.style.borderLeft = "1px solid black";
+            row.style.borderRight = "1px solid black";
+        } else {
+            row.style.borderLeft = '1px solid black';
+        }
+        row.style.borderTop = '1px solid black';
+        //Set Bottom Border only for last items in each column
+        if (j === 15) {
+            row.style.borderBottom = '1px solid black';
+        }
         
         column.appendChild(row);
      }
